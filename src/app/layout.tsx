1f30aea5aac1e3982db3818f8234362429215bba
Paw565pl <paw565pl@gmail.com>
2024-07-08 12:41:03 +0200

    chore: setup auth.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Providers from "@/components/providers";
 import type { Metadata } from "next";
 import { ReactNode } from "react";
 import "./globals.css";
@@ -13,7 +14,9 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
