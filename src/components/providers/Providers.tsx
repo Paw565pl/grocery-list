a326bd380d201e212c13bd1a1176ba5a66440927
Paw565pl <paw565pl@gmail.com>
2024-07-08 22:40:43 +0200

    chore: setup tanstack query

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,14 +1,37 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import ms from "ms";
 import { SessionProvider } from "next-auth/react";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 const Providers = ({ children }: ProvidersProps) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: ms("1m"),
+          },
+        },
+      }),
+  );
+
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </SessionProvider>
+  );
 };
 
 export default Providers;
