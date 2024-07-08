408802033d53d1fe4d82274536cb65bb28d4df16
Paw565pl <paw565pl@gmail.com>
2024-07-08 18:13:31 +0200

    fix: provide correct auth url

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import Auth0 from "next-auth/providers/auth0";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Auth0],
+  providers: [Auth0({ authorization: process.env.AUTH_AUTH0_AUTH_URL })],
   callbacks: {
     jwt: ({ token, account, profile }) => {
       if (account && profile) {
