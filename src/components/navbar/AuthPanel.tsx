1d666b6fb5b92da3d6e1751b7be684466ac64217
Paw565pl <paw565pl@gmail.com>
2024-07-08 13:06:31 +0200

    feat: create auth panel

---
+++ src/components/navbar/AuthPanel.tsx
@@ -0,0 +1,18 @@
+import { auth } from "@/auth";
+import SignInButton from "./SignInButton";
+import SignOutButton from "./SignOutButton";
+
+const AuthPanel = async () => {
+  const session = await auth();
+
+  if (!session) return <SignInButton />;
+
+  return (
+    <div className="flex items-center gap-2">
+      <span>{session.user?.name}</span>
+      <SignOutButton />
+    </div>
+  );
+};
+
+export default AuthPanel;
