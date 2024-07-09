0a9e9e20baaa62428dc937581546e503076ba48b
Paw565pl <paw565pl@gmail.com>
2024-07-09 11:32:54 +0200

    fix: add cache to axios requests

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -1,11 +1,12 @@
 import Category from "@/entities/category";
 import apiService from "@/services/apiService";
 import { QueryClient, useQuery } from "@tanstack/react-query";
+import { cache } from "react";
 
-const fetchCategories = async () => {
+const fetchCategories = cache(async () => {
   const { data } = await apiService.get<Category[]>("/categories");
   return data;
-};
+});
 
 const queryKey = ["categories"];
 
--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,17 +1,20 @@
 import Product from "@/entities/product";
 import apiService from "@/services/apiService";
 import { QueryClient, useQuery } from "@tanstack/react-query";
+import ms from "ms";
+import { cache } from "react";
 
-const fetchProducts = async () => {
+const fetchProducts = cache(async () => {
   const { data } = await apiService.get<Product[]>("/products");
   return data;
-};
+});
 
 const queryKey = ["products"];
 
 export const prefetchProducts = (queryClient: QueryClient) =>
   queryClient.prefetchQuery({ queryKey, queryFn: fetchProducts });
 
-const useProducts = () => useQuery({ queryKey, queryFn: fetchProducts });
+const useProducts = () =>
+  useQuery({ queryKey, queryFn: fetchProducts, staleTime: ms("10m") });
 
 export default useProducts;
