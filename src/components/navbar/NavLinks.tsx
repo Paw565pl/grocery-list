e45598a1e7848a0276c8f3c79d13c4d3494c2264
Paw565pl <paw565pl@gmail.com>
2024-07-09 11:09:38 +0200

    feat: create nav links

---
+++ src/components/navbar/NavLinks.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavLinks = () => {
+  const pathname = usePathname();
+  const links = [
+    { label: "Products", href: "/products" },
+    { label: "Complaint", href: "/complaint" },
+    { label: "Admin", href: "/admin" },
+  ];
+
+  return (
+    <ul className="ml-10 flex space-x-8">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            href={link.href}
+            className={
+              link.href === pathname
+                ? "text-zinc-800"
+                : "text-zinc-700 hover:text-blue-500"
+            }
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavLinks;
