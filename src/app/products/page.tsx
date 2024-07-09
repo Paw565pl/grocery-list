bd345f448f3e994f54f2ffcd2c16c98380224cfc
Paw565pl <paw565pl@gmail.com>
2024-07-09 15:27:33 +0200

    feat: add prefetching to products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,22 @@
 import ProductsTable from "@/components/productsTable";
+import { prefetchProducts } from "@/hooks/useProducts";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
 export const revalidate = 600; // 10 minutes
 
-const ProductsPage = () => {
+const ProductsPage = async () => {
+  const queryClient = new QueryClient();
+  await prefetchProducts(queryClient);
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <h1 className="text-5xl font-bold">Products</h1>
       <ProductsTable />
-    </>
+    </HydrationBoundary>
   );
 };
 
