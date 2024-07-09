e0f59cc4726dcef880bb92edcbe3d91ae69ffddb
Paw565pl <paw565pl@gmail.com>
2024-07-09 11:13:36 +0200

    feat: create welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="space-y-4">
+      <h1 className="text-5xl font-bold">Home Page</h1>
+      <p>Welcome to our store!</p>
+    </div>
+  );
 };
 
 export default HomePage;
