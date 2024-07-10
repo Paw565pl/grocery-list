9777813dd478566544f8c05e3a6e0038b8846d22
Paw565pl <paw565pl@gmail.com>
2024-07-10 22:08:17 +0200

    test: add mocked services to test setup

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,25 @@
+import { drop } from "@mswjs/data";
 import "@testing-library/jest-dom/vitest";
+import db from "./mocks/db";
+import server from "./mocks/server";
+
+beforeAll(() => server.listen());
+afterEach(() => {
+  drop(db);
+  server.resetHandlers();
+});
+afterAll(() => server.close());
+
+vi.mock("react", async () => {
+  const testCache = <T extends (...args: Array<unknown>) => unknown>(func: T) =>
+    func;
+  const originalModule = await vi.importActual("react");
+  return {
+    ...originalModule,
+    cache: testCache,
+  };
+});
+
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
+window.HTMLElement.prototype.hasPointerCapture = vi.fn();
+window.HTMLElement.prototype.releasePointerCapture = vi.fn();
