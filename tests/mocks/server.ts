da94f5b8cc72910f044172837769bd1077b0dc59
Paw565pl <paw565pl@gmail.com>
2024-07-10 22:07:37 +0200

    test: setup mock server

---
+++ tests/mocks/server.ts
@@ -0,0 +1,28 @@
+import { delay, HttpRequestHandler, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+import db from "./db";
+
+export const simulateApiError = (
+  endpoint: string,
+  method: HttpRequestHandler,
+) => server.use(method(endpoint, () => HttpResponse.error()));
+
+export const simulateApiDelay = (
+  endpoint: string,
+  method: HttpRequestHandler,
+) =>
+  server.use(
+    method(endpoint, async () => {
+      await delay();
+      return HttpResponse.json([]);
+    }),
+  );
+
+const handlers = [
+  ...db.category.toHandlers("rest"),
+  ...db.product.toHandlers("rest"),
+];
+
+const server = setupServer(...handlers);
+
+export default server;
