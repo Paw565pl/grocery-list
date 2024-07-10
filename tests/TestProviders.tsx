cb931cb024a6a111e4961bea74aaaf62dce79d72
Paw565pl <paw565pl@gmail.com>
2024-07-10 22:07:54 +0200

    test: create test providers component

---
+++ tests/TestProviders.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+
+const TestProviders = ({ children }: PropsWithChildren) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default TestProviders;
