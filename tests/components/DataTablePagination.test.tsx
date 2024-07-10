33cd73c98d22fcccf1a92c338588e7c036edabd2
Paw565pl <paw565pl@gmail.com>
2024-07-10 22:05:40 +0200

    fix: update data table pagination tests

--- tests/components/DataTablePagination.test.tsx
+++ tests/components/DataTablePagination.test.tsx
@@ -3,53 +3,38 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe("DataTablePagination", () => {
-  const getElements = () => ({
-    user: userEvent.setup(),
-    previousButton: screen.getByRole("button", { name: /previous/i }),
-    nextButton: screen.getByRole("button", { name: /next/i }),
-  });
-
   it("should render disabled previous button if no previous page exists", () => {
     const previousPage = vi.fn();
-    render(
-      <DataTablePagination
-        nextPage={vi.fn()}
-        previousPage={previousPage}
-        getCanNextPage={vi.fn()}
-        getCanPreviousPage={vi.fn().mockReturnValue(false)}
-      />,
-    );
+    render(<DataTablePagination {...getProps()} previousPage={previousPage} />);
 
     const { previousButton } = getElements();
 
-    expect(previousPage).not.toBeCalled();
     expect(previousButton).toBeDisabled();
+    expect(previousPage).not.toBeCalled();
   });
 
   it("should render disabled next button if no next page exists", () => {
     const nextPage = vi.fn();
     render(
       <DataTablePagination
+        {...getProps()}
         nextPage={nextPage}
-        previousPage={vi.fn()}
         getCanNextPage={vi.fn().mockReturnValue(false)}
-        getCanPreviousPage={vi.fn()}
       />,
     );
 
     const { nextButton } = getElements();
 
-    expect(nextPage).not.toBeCalled();
     expect(nextButton).toBeDisabled();
+    expect(nextPage).not.toBeCalled();
   });
 
   it("should call previousPage callback if previous button is clicked", async () => {
     const previousPage = vi.fn();
     render(
       <DataTablePagination
-        nextPage={vi.fn()}
+        {...getProps()}
         previousPage={previousPage}
-        getCanNextPage={vi.fn()}
         getCanPreviousPage={vi.fn().mockReturnValue(true)}
       />,
     );
@@ -58,18 +43,17 @@ describe("DataTablePagination", () => {
 
     await user.click(previousButton);
 
-    expect(previousPage).toBeCalled();
     expect(previousButton).toBeEnabled();
+    expect(previousPage).toHaveBeenCalledOnce();
   });
 
   it("should call nextPage callback if next button is clicked", async () => {
     const nextPage = vi.fn();
     render(
       <DataTablePagination
+        {...getProps()}
         nextPage={nextPage}
-        previousPage={vi.fn()}
         getCanNextPage={vi.fn().mockReturnValue(true)}
-        getCanPreviousPage={vi.fn()}
       />,
     );
 
@@ -77,7 +61,25 @@ describe("DataTablePagination", () => {
 
     await user.click(nextButton);
 
-    expect(nextPage).toBeCalled();
     expect(nextButton).toBeEnabled();
+    expect(nextPage).toHaveBeenCalledOnce();
+  });
+
+  const state = { pagination: { pageIndex: 0 } };
+  const getElements = () => ({
+    user: userEvent.setup(),
+    previousButton: screen.getByRole("button", { name: /previous/i }),
+    nextButton: screen.getByRole("button", { name: /next/i }),
+    currentPage: screen.getByText(/\d+/),
+    totalPages: screen.getByText(/\d+/),
+  });
+
+  const getProps = () => ({
+    nextPage: vi.fn(),
+    previousPage: vi.fn(),
+    getCanNextPage: vi.fn(),
+    getCanPreviousPage: vi.fn(),
+    getState: vi.fn().mockReturnValue(state),
+    getPageCount: vi.fn(),
   });
 });
