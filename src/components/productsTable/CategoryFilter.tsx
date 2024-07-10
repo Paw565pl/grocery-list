27cc4ca6934b324e3612a7480056d6c727d2da71
Paw565pl <paw565pl@gmail.com>
2024-07-10 14:01:11 +0200

    feat: create category filter component

---
+++ src/components/productsTable/CategoryFilter.tsx
@@ -0,0 +1,39 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import useCategories from "@/hooks/useCategories";
+
+interface CategoryFilterProps {
+  onValueChange: (value: string) => void;
+}
+
+const CategoryFilter = ({ onValueChange }: CategoryFilterProps) => {
+  const { data: categories, isError, isLoading } = useCategories();
+
+  if (isLoading || isError) return null;
+
+  return (
+    <Select onValueChange={onValueChange}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Filter by category" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value="all">All</SelectItem>
+          {categories?.map((category) => (
+            <SelectItem key={category.id} value={category.id.toString()}>
+              {category.name}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default CategoryFilter;
