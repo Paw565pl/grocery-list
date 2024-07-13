38509b535137d1be348e498435f6507882be3090
Paw565pl <paw565pl@gmail.com>
2024-07-13 15:37:00 +0200

    chore: create mock for authentication

--- tests/setup.ts
+++ tests/setup.ts
@@ -20,6 +20,28 @@ vi.mock("react", async () => {
   };
 });
 
+// mock server session
+vi.mock("@/auth", async () => {
+  const originalModule = await vi.importActual("@/auth");
+
+  const auth = vi.fn().mockResolvedValue(null);
+
+  return { ...originalModule, auth };
+});
+
+// mock client session
+vi.mock("next-auth/react", async () => {
+  const originalModule = await vi.importActual("next-auth/react");
+
+  const useSession = vi.fn().mockReturnValue({
+    data: null,
+    status: "unauthenticated",
+    update: vi.fn(),
+  });
+
+  return { ...originalModule, useSession };
+});
+
 window.HTMLElement.prototype.scrollIntoView = vi.fn();
 window.HTMLElement.prototype.hasPointerCapture = vi.fn();
 window.HTMLElement.prototype.releasePointerCapture = vi.fn();
